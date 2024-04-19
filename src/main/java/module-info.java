53bd4826b1f61946ca86f34fe99b368318cdbda0
georgesroberto <georgesroberto21@gmail.com>
2024-04-19 18:00:01 +0300

    Add module for transitive javafx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module uk.ac.soton.comp1206 {
     requires javafx.media;
     requires org.apache.logging.log4j;
     requires nv.websocket.client;
+    requires transitive javafx.base;
     opens uk.ac.soton.comp1206.ui to javafx.fxml;
     exports uk.ac.soton.comp1206;
     exports uk.ac.soton.comp1206.ui;
